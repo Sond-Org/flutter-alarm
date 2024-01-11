b1db7e3d8d6e52ac4323c31fa2bd150bd01dbf7d
Amir Lazarovich <amir@somnius.io>
2024-01-11 13:40:52 -0500

    fix: avoid calling non-existing native ios code

--- lib/platform/ios_alarm.dart
+++ lib/platform/ios_alarm.dart
@@ -1,6 +1,37 @@
+import 'package:alarm/model/alarm_settings.dart';
 import 'package:alarm/platform/base_alarm.dart';
 
 /// iOS implementation of the alarm plugin.
 ///
 /// NOTE: Native-side not implemented yet.
-class IOSAlarm extends BaseAlarm {}
+class IOSAlarm extends BaseAlarm {
+  @override
+  Future<bool> set(AlarmSettings settings) {
+    return Future.value(false);
+  }
+
+  @override
+  Future<bool> snooze(int id) {
+    return Future.value(false);
+  }
+
+  @override
+  Future<bool> stop(int id) async {
+    return Future.value(false);
+  }
+
+  @override
+  Future<bool> isRinging(int id) async {
+    return Future.value(false);
+  }
+
+  @override
+  Future<List<int>> getRingingIds() async {
+    return Future.value([]);
+  }
+
+  @override
+  Future<void> stopNotificationOnKillService() {
+    return Future.value();
+  }
+}
